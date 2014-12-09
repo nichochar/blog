5ffd8aba52b7c9965c435525a6ab78a527f7ac5c
Nicholas Charriere <nicholascharriere@gmail.com>
2014-12-09 00:05:47 -0800

    Added article on window management and tweaked style

--- nicho_website/articles.py
+++ nicho_website/articles.py
@@ -7,6 +7,13 @@ articles_data = [
         'description': 'This is just a test for the first post of my blog'
     },
     {
+        'slug': 'basic-slate',
+        'year': 2014,
+        'month': 12,
+        'title': 'Quick install of Slate, the OSX window manager',
+        'description': 'Window managers are more and more popular. They make you smarter, more beautiful, and overall a better person. Here\'s a quick tutorial as to how to install the open source Slate'
+    },
+    {
         'slug': 'first-code-snippets-in-7-languages',
         'year': 2014,
         'month': 6,
@@ -23,4 +30,5 @@ articles_data = [
         'description': 'TED talks are awesome, but there\'s a lot of them. \
                 this is a short selection of a few I like.'
     }
+
     ]
