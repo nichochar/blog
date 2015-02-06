f4e8fde0b0aa6f7790fd419e73bb58e1d33eddaf
Nicholas Charriere <nicholascharriere@gmail.com>
2015-02-06 13:50:27 -0800

    hid some articles and updated CV

--- blog/articles.py
+++ blog/articles.py
@@ -13,22 +13,22 @@ articles_data = [
         'title': 'Quick install of Slate, the OSX window manager',
         'description': 'Window managers are more and more popular. They make you smarter, more beautiful, and overall a better person. Here\'s a quick tutorial as to how to install the open source Slate'
     },
-    {
-        'slug': 'first-code-snippets-in-7-languages',
-        'year': 2014,
-        'month': 6,
-        'title': 'Code snippets in 7 languages',
-        'description': 'My first code snippets in 7 different languages, \
-                done during the reading of the book seven languages in \
-                seven weeks'
-    },
-    {
-        'slug': 'ted-talks-selection',
-        'year': 2014,
-        'month': 9,
-        'title': 'Favorite TED talks',
-        'description': 'TED talks are awesome, but there\'s a lot of them. \
-                this is a short selection of a few I like.'
-    }
+    # {
+        # 'slug': 'first-code-snippets-in-7-languages',
+        # 'year': 2014,
+        # 'month': 6,
+        # 'title': 'Code snippets in 7 languages',
+        # 'description': 'My first code snippets in 7 different languages, \
+                # done during the reading of the book seven languages in \
+                # seven weeks'
+    # },
+    # {
+        # 'slug': 'ted-talks-selection',
+        # 'year': 2014,
+        # 'month': 9,
+        # 'title': 'Favorite TED talks',
+        # 'description': 'TED talks are awesome, but there\'s a lot of them. \
+                # this is a short selection of a few I like.'
+    # }
 
     ]
