78c2034bc96adc63f3b6c6b243be33f5dbad6704
Nicholas Charriere <nicholascharriere@gmail.com>
2015-05-18 11:53:01 -0700

    Added new article for changing jobs

--- blog/articles.py
+++ blog/articles.py
@@ -13,22 +13,11 @@ articles_data = [
         'title': 'Quick install of Slate, the OSX window manager',
         'description': 'Window managers are awesome. They make you smarter, more beautiful, and overall a better person. Tutorial to get you setup fast.'
     },
-    # {
-        # 'slug': 'first-code-snippets-in-7-languages',
-        # 'year': 2014,
-        # 'month': 6,
-        # 'title': 'Code snippets in 7 languages',
-        # 'description': 'My first code snippets in 7 different languages, \
-                # done during the reading of the book seven languages in \
-                # seven weeks'
-    # },
-    # {
-        # 'slug': 'ted-talks-selection',
-        # 'year': 2014,
-        # 'month': 9,
-        # 'title': 'Favorite TED talks',
-        # 'description': 'TED talks are awesome, but there\'s a lot of them. \
-                # this is a short selection of a few I like.'
-    # }
-
-    ]
+    {
+        'slug': 'changing-jobs',
+        'year': 2015,
+        'month': 5,
+        'title': 'Moving Forward',
+        'description': 'After more than 2 years at StarMaker, it\'s time to move on to the next adventure.'
+    },
+]
